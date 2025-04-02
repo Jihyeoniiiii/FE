59f0fd65e8a9a91490adc8d4aacf490983c2b7da
Jihyeoniiiii <qwere4585@naver.com>
2025-04-02 02:26:43 +0000

    refactor: 스크롤과 네비바 문제 해결

--- src/app/(funding)/funding/page.tsx
+++ src/app/(funding)/funding/page.tsx
@@ -10,14 +10,17 @@ export default function FundingListPage() {
       <FilterButtons />
 
       {/* Projects grid */}
-      <div className="relative top-[6vh] mt-[10px] grid grid-cols-2 gap-3 max-h-[80vh] overflow-y-auto scrollbar-none">
+      <div className="relative top-[6vh] mt-[10px] grid grid-cols-2 gap-3 max-h-[66vh] overflow-y-scroll scrollbar-none">
+        {transformedCampaigns.map((campaign) => (
+          <CampaignCard key={campaign.id} campaign={campaign} />
+        ))}
         {transformedCampaigns.map((campaign) => (
           <CampaignCard key={campaign.id} campaign={campaign} />
         ))}
       </div>
 
       {/* Floating Button */}
-      <div className="flex justify-end mt-30">
+      <div className="flex justify-end mt-19">
         <NavigateToWriteButton />
       </div>
     </div>
