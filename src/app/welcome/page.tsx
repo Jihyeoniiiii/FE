5e0a2a1f2af9262146c0b6eb16fe5b6455bdb985
Jihyeoniiiii <qwere4585@naver.com>
2025-05-14 10:38:32 +0000

    BugFix : build에러 해결 (#94)

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -26,7 +26,8 @@ export default function WelcomePage() {
     };
 
     getUser();
-  }, []);
+  }, [setUserData
+  ]);
 
   return <ClientWelcome />;
 }
--- src/components/payment/PointContainer.tsx
+++ src/components/payment/PointContainer.tsx
@@ -1,26 +1,8 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import axiosInstance from "@/lib/api/axios";
-import { useQuery } from "@tanstack/react-query";
 
 const PointContainer = () => {
-  const [point, setPoint] = useState<number>(0);
-  const fetchPoints = async () => {
-    const res = await axiosInstance.get("/api/v1/member/point");
-
-    return res.data;
-  };
-  const {
-    data: points,
-    isPending,
-    isError,
-  } = useQuery({
-    queryKey: ["points"],
-    queryFn: fetchPoints,
-  });
   return (
     <div className="flex justify-center items-center">
       <div className="w-[80%] h-[100px] bg-white/50 border-2 border-white rounded-2xl">
