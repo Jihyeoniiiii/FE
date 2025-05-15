142c6a7be19b7f2b9fbdf434e2caaf7cf3e1c0dc
Jihyeoniiiii <qwere4585@naver.com>
2025-05-15 09:13:22 +0000

    api : api 연동하는 axio 함수식 구현(#101)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
+"use client";
+
 import BottomMenu from "@/components/main/BottomMenu";
 import ProgressBar from "@/components/main/ProgressBar";
 import TopMenu from "@/components/main/TopMenu";
 import NavigationBar from "@/components/ui/NavigationBar";
+import axiosInstance from "@/lib/api/axios";
+import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
 
 export default function Home() {
+  const fetchCatInfo = async () => {
+    const { data } = await axiosInstance.get("/api/v1/cat");
+    console.log(data.data);
+    return data.data;
+  };
+
+  const { data: catInfo } = useQuery({
+    queryKey: ["cat"],
+    queryFn: fetchCatInfo,
+  });
+
   return (
     <div className="flex flex-col h-screen">
       <TopMenu />
       <div className="flex flex-col justify-between flex-grow pb-[8rem]">
         <div className="flex flex-col py-5">
-          <ProgressBar now={35} />
+          {catInfo && <ProgressBar catData={catInfo} />}
         </div>
         <div className="flex justify-center">
           <Image
--- src/components/main/ProgressBar.tsx
+++ src/components/main/ProgressBar.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 interface ProgressBarProps {
-  now: number;
+  catData: {
+    id: number;
+    level: number;
+    experiencePoint: number;
+    levelUpPercentage: number;
+  };
 }
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ now }) => {
-  const clampedNow = Math.max(0, Math.min(100, now));
-  const widthPercent = `${clampedNow}%`;
+const ProgressBar: React.FC<ProgressBarProps> = ({ catData }) => {
+  const widthPercent = `${catData.levelUpPercentage}%`;
 
   return (
     <div className="flex flex-col gap-1.5">
@@ -14,19 +18,17 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ now }) => {
         <div
           className="flex w-[80%] h-4 bg-gray-200 rounded-full overflow-hidden"
           role="progressbar"
-          aria-valuenow={Number(clampedNow)}
+          aria-valuenow={Number(catData.levelUpPercentage)}
           aria-valuemin={Number(0)}
-          aria-valuemax={Number(100)}
-        >
+          aria-valuemax={Number(100)}>
           <div
             className="flex justify-center h-4 rounded-full overflow-hidden bg-primary whitespace-nowrap transition duration-500"
-            style={{ width: widthPercent }}
-          ></div>
+            style={{ width: widthPercent }}></div>
         </div>
       </div>
       <div className="flex px-10 sm:px-13 md:px-16 justify-between">
         <span className="flex px-3 bg-white text-sm font-semibold items-center rounded-2xl">
-          레벨1
+          레벨{catData.level}
         </span>
         <span className="text-sm font-semibold">{widthPercent}</span>
       </div>
