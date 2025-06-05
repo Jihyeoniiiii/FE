342b1a18719a3487db9c7bb85007b8786f554f7e
Jihyeoniiiii <qwere4585@naver.com>
2025-06-05 06:05:07 +0000

    design: 보상 status bar 색상 적용

--- src/components/BodyBackgroundHandler.tsx
+++ src/components/BodyBackgroundHandler.tsx
@@ -7,13 +7,19 @@ export default function BodyBackgroundHandler() {
   const pathname = usePathname();
 
   useEffect(() => {
+    const metaThemeColor = document.querySelector("meta[name='theme-color']");
+
     if (
       pathname === "/donation/reward" ||
       pathname === "/donation/reward/notify"
     ) {
       document.body.classList.add("bg-white");
+      document.documentElement.style.backgroundColor = "#ffffff";
+      metaThemeColor?.setAttribute("content", "#ffffff");
     } else {
       document.body.classList.remove("bg-white");
+      document.documentElement.style.backgroundColor = "#E2E3FC";
+      metaThemeColor?.setAttribute("content", "#E2E3FC");
     }
   }, [pathname]);
 
